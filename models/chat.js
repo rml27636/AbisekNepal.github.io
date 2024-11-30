9c7aeaf36b30e462e13155c68109a293ee3d4e04
rml27636 <57963901+rml27636@users.noreply.github.com>
2024-11-30 12:22:09 -0500

    chat.js

--- models/chat.js
+++ models/chat.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
 const ChatSchema = new mongoose.Schema({
-  roomId: { type: String, required: true },  // Room ID (e.g., itemId or userId)
+  roomId: { type: String, required: true },  // Room ID (unique to renter-owner pair)
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],  // Owner and Renter IDs
   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // Sender's user ID
   message: { type: String, required: true },  // The chat message
   createdAt: { type: Date, default: Date.now },  // Timestamp
